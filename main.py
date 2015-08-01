ee498fd03583f60c1de6d15399f26a73dc145dcf
Flavio Figueiredo <flaviovdf@gmail.com>
2015-08-01 12:17:01 -0300

    Fixed bug in main for mpi

--- main.py
+++ main.py
@@ -55,37 +55,6 @@ def main():
     
     args = parser.parse_args()
     
-    num_lines = 0
-    with open(args.trace_fpath) as trace_file:
-        num_lines = sum(1 for _ in trace_file)
-    
-    if args.leaveout > 0:
-        leave_out = min(1, args.leaveout)
-        if leave_out == 1:
-            print('Leave out is 1 (100%), nothing todo')
-            return
-        from_ = 0
-        to = int(num_lines - num_lines * leave_out)
-    else:
-        from_ = 0
-        to = np.inf
-
-    if args.alpha_zh is None:
-        alpha_zh = 50.0 / args.num_topics
-    else:
-        alpha_zh = args.alpha_zh
-    
-    kernel = kernels.names[args.kernel]()
-    if args.residency_priors:
-        residency_priors = np.array(args.residency_priors, dtype='d')
-    else:
-        residency_priors = np.zeros(shape=(0, ), dtype='d')
-    
-    if np.isinf(to):
-        kernel.build(num_lines - from_, args.num_topics, residency_priors)
-    else:
-        kernel.build(to - from_, args.num_topics, residency_priors)
-    
     comm = MPI.COMM_WORLD
     rank = comm.rank
     size = comm.size 
@@ -96,20 +65,53 @@ def main():
         single_thread = size == 1
     except:
         single_thread = True
-
-    if single_thread:
-        print('Not on MPI mode or just one MPI proc, running single thread')
-        single_thread = True
-
-    if single_thread:
-        rv = learn.fit(args.trace_fpath, args.num_topics, alpha_zh, \
-                args.beta_zs, args.beta_zd, kernel, residency_priors, \
-                args.num_iter, args.burn_in, from_=from_, to=to)
-        dataio.save_model(args.model_fpath, rv)
+    
+    if not single_thread and rank != plearn.MASTER:
+        plearn.work()
     else:
-        comm = MPI.COMM_WORLD
-        rank = comm.rank
-        if rank == plearn.MASTER:
+        num_lines = 0
+        with open(args.trace_fpath) as trace_file:
+            num_lines = sum(1 for _ in trace_file)
+        
+        if args.leaveout > 0:
+            leave_out = min(1, args.leaveout)
+            if leave_out == 1:
+                print('Leave out is 1 (100%), nothing todo')
+                return
+            from_ = 0
+            to = int(num_lines - num_lines * leave_out)
+        else:
+            from_ = 0
+            to = np.inf
+
+        if args.alpha_zh is None:
+            alpha_zh = 50.0 / args.num_topics
+        else:
+            alpha_zh = args.alpha_zh
+        
+        kernel = kernels.names[args.kernel]()
+        if args.residency_priors:
+            residency_priors = np.array(args.residency_priors, dtype='d')
+        else:
+            residency_priors = np.zeros(shape=(0, ), dtype='d')
+        
+        if np.isinf(to):
+            kernel.build(num_lines - from_, args.num_topics, residency_priors)
+        else:
+            kernel.build(to - from_, args.num_topics, residency_priors)
+        
+        if single_thread:
+            print('Not on MPI mode or just one MPI proc, running single thread')
+            single_thread = True
+
+        if single_thread:
+            rv = learn.fit(args.trace_fpath, args.num_topics, alpha_zh, \
+                    args.beta_zs, args.beta_zd, kernel, residency_priors, \
+                    args.num_iter, args.burn_in, from_=from_, to=to)
+            dataio.save_model(args.model_fpath, rv)
+        else:
+            comm = MPI.COMM_WORLD
+            rank = comm.rank
             dyn = args.dynamic
             if dyn:
                 num_iter = args.num_iter // args.num_batches
@@ -121,8 +123,6 @@ def main():
                         args.beta_zs, args.beta_zd, kernel, residency_priors, \
                         args.num_iter, from_=from_, to=to)
             dataio.save_model(args.model_fpath, rv)
-        else:
-            plearn.work()
 
 if __name__ == '__main__':
     main()
