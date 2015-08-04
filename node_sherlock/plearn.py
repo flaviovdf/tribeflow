468d6f2c12766a89eccf73a1eca38141a39f3fee
Flavio Figueiredo <flaviovdf@gmail.com>
2015-08-04 09:22:17 -0300

    Removed print function

--- node_sherlock/plearn.py
+++ node_sherlock/plearn.py
@@ -419,7 +419,6 @@ def fit(trace_fpath, num_topics, alpha_zh, beta_zs, beta_zd, kernel, \
     
     workloads = generate_workload(Count_zh.shape[1], num_workers, Trace)
     for worker_id in xrange(1, num_workers + 1):
-        print(worker_id, num_iter)
         comm.send(num_iter, dest=worker_id, tag=Msg.LEARN.value)
     
     dispatch_jobs(tstamps, Trace, Count_zh, Count_sz, Count_dz, count_h, \
